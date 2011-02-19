297a93c22f58d23eb20009305f57d4b0c0289d74
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-02-19 18:06:36 -0500

    more includes for head

--- Support/head.php
+++ Support/head.php
@@ -1,5 +1,7 @@
 <?php
+// All the required files we will need
+
+include getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR.'ui.php';
 include getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR.'config.php';
 
-
-
+include getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR.'mailchimp'.DIRECTORY_SEPARATOR.'MCAPI.class.php';
