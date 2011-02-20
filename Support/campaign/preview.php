df623f2d0c532bf55b3fd5a0ea5927e768f3c8d7
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-02-20 17:00:10 -0500

    Add preview functionality

---
+++ Support/campaign/preview.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Preview Archive Version of campaign
+ *
+ * @author Mitchell Amihod
+ */
+
+//Get RAW content
+//so, archived view shows what it would look like on the website oi guess ?
+$retval = $api->campaignContent($config->campaign_id, true);
+$oopsy->go($api->errorCode, $api->errorMessage, 'Problem getting Preview Archive.');
+
+
+
+echo ('html' == $id) ? $retval['html'] : $retval['text'];
+
