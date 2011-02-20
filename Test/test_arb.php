a1e3ebdd53b1768bbbb333f7e0a5333b41a1511b
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-02-20 11:15:51 -0500

    testing

---
+++ Test/test_arb.php
@@ -0,0 +1,5 @@
+<?php
+include getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR.'head.php';
+
+$retval = $api->campaigns();
+var_dump($retval);
---
+++ Test/test_upload.php
@@ -0,0 +1,4 @@
+<?php
+include getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR.'head.php';
+
+
