e706802656e90ad23b92847264f6c374c2bb0568
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-03-02 00:01:22 -0500

    make info handle errors same as anyone else

--- Support/campaign/info.php
+++ Support/campaign/info.php
@@ -6,9 +6,9 @@
 $retval = $api->campaigns(array('campaign_id'=>$config->campaign_id));
 
 if(false == $retval) {
-    echo "I'm sorry, but it seems there is a problem with getting info about this campaign.".
-            "<br>Are you sure you have setup your config?";
-    return;
+    $err_msg = "I'm sorry, but it seems there is a problem with getting info about this campaign.".
+               "<br>Are you sure you have setup your config?";
+    $oopsy->go(999, $err_msg);
 }
 
 $ignoreFields = array('tracking', 'segment_opts', 'segment_text', 'type_opts' );
