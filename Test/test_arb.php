384ead344b8f388079823e435e7ea7665fe049c8
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-03-01 23:27:08 -0500

    arb test working!

--- Test/test_arb.php
+++ Test/test_arb.php
@@ -1,5 +1,36 @@
 <?php
 include getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR.'head.php';
 
-$retval = $api->templates(array('user'=>false,'gallery'=>false,'base'=>true ));
+$exclusion_list = getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR . 'tar_exclusion_list.txt';
+
+$to_tar = getenv('TM_PROJECT_DIRECTORY');
+
+// echo sys_get_temp_dir();
+// $tar_destination = tempnam(string dir, string prefix)
+$tar_file = sys_get_temp_dir().DIRECTORY_SEPARATOR.'mc_to_upload.tgz';
+
+$tarcmd = "tar --exclude-from='{$exclusion_list}' -cvzf {$tar_file} {$to_tar}";
+
+//exec the command
+echo `$tarcmd`;
+
+$content = base64_encode(file_get_contents($tar_file));
+
+// $handle = fopen($tar_file, "r");
+
+// $content_binary = fread(fopen($imgfile, "r"), filesize($imgfile));
+
+// $content = fread(fopen($imgfile, "r"), filesize($imgfile));
+
+// echo '<img src="data:image/gif;base64,' . base64_encode($imgbinary) . '" />';
+
+$retval = $api->campaignUpdate($config->campaign_id, 'content', array('archive'=>$content, 'archive_type'=>'tgz'));
 var_dump($retval);
+$oopsy->go($api->errorCode, $api->errorMessage, "Problem uploading {$tar_file}");
+
+
+
+// We will declare a special directory: _local - this is where you can keep any notes, work, etc
+// it will be skipped when we upload the folder
+// tar --exclude="_local" -cvzf test_zip_tar.tgz test_to_zip
+//$zipresult = `tar -cvzf test_zip_tar.tgz test_to_zip`;
