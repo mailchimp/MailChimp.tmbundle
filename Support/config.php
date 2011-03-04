1889c3b77f0e773e9fcb0f11810e5822981bc78a
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-03-03 23:11:26 -0500

    add template id

--- Support/config.php
+++ Support/config.php
@@ -29,6 +29,9 @@ class mConfig {
     var $campaign_id = null;
     var $list_id = null;
     
+    // Currently useful in the context of working with Templates - not campaigns.
+    var $template_id = null;
+    
     function __construct($path) {
 
         if(!file_exists($path)) {
@@ -47,9 +50,10 @@ class mConfig {
      * @return void
      **/
     public function save() {
-        $to_write = array('api_key', 'campaign_id', 'list_id');
+        $to_write = array('api_key', 'campaign_id', 'list_id', 'template_id');
         $output = '';
         foreach ($to_write as $key) {
+            if(is_null($this->{$key})) { continue; }
             $output.= $this->_ini_line($key, $this->{$key}, true);
         }
         
