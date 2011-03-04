ded2d3b17f7d0bc33b6633c0adfcf94a5529bd87
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-03-03 22:26:32 -0500

    Expand do to allow choice of where to pull a template from. so now you can browse gallery, base, or user

--- Support/template/do.php
+++ Support/template/do.php
@@ -6,21 +6,36 @@
  * So, roll into one for now to show what's possible,
  *
  * Hmm, a bit annoying, need to specify user/base/gallery when passing in template_id to templateInfo() 
- * Will have to rethink
- * Hardcoded to base for now
- *
- * By default, will output the 
+ * 
+ * I think this is an infrequent enough operation that its not onerous to have to pick from more than one menu.
  *
  * @author Mitchell Amihod
  */
 
 $UI = new UI(getenv('DIALOG'));
 
-$retval = $api->templates(array('user'=>false,'gallery'=>false,'base'=>true ));
+//Begin Template Type Selection
+$template_options = array('user'=>false,'gallery'=>false,'base'=>false );
+
+$temp_choices = array();
+foreach (array_keys($template_options) as $temp_type) {
+    $temp = '{title="%s";id="%s";}';
+    $temp_choices[] = sprintf($temp, ucfirst($temp_type), $temp_type);
+}
+$response = $UI->menu($temp_choices);
+if(empty($response)) {
+    exit( 'Cancelled.');
+}
+$xml = new SimpleXMLElement($response);
+$template_type = $tool->getValue($xml, 'id');
+$template_options[$template_type] = true;
+////////End Template Type User Choice Selection
+
+$retval = $api->templates($template_options);
 $oopsy->go($api->errorCode, $api->errorMessage, 'Problem templates do.');
 
 // $templates = array_merge($retval['user'], $retval['base']);
-$templates = $retval['base'];
+$templates = $retval[$template_type];
 
 $collector = array();
 foreach($templates as $template){
@@ -36,7 +51,7 @@ if(empty($response)) {
 $xml = new SimpleXMLElement($response);
 $template_id = $tool->getValue($xml, 'id');
 
-$template_info = $api->templateInfo($template_id, 'base');
+$template_info = $api->templateInfo($template_id, $template_type);
 $oopsy->go($api->errorCode, $api->errorMessage, 'Problem Template Info');
 
 switch (true) {
