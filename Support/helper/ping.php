fb08fe9968fa5e00952faac13bef87e96820887a
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-03-10 23:40:12 -0500

    Minor tweaks Add some fun to the ping command

--- Support/helper/ping.php
+++ Support/helper/ping.php
@@ -1,3 +1,66 @@
 <?php 
 $retval = $api->ping();
 echo $retval;
+?>
+<pre> 
+                       .;fDDEEEEKEKKKKEKEGi..               
+                       tGEEEEEEEKEEKEKKKKEGj;               
+                      :LEDEEEEKDEEEKEKKKKKKDL;              
+                     ;LDDDDDDEEKDEDEEEKKKKWKKEj             
+                    :GEEDDEDEDEKGGEDEDEKKKWWKKEi,,.         
+                   ;DDEDEEEDEEDDGGGDDDDEEKWWWWKKDGf;.       
+                 ,fEEEEEEDGDGDLLLGGGLGGGEEKKWWKKKDj:        
+                 iGEEEEEDGLLLLfffLfLLjLGGEEKKKWWKEDft       
+                ;GEEEEEDGLjfjjjtjitffGGLGGDKKKWWWELi.       
+                :fDEEDDLLffftii;t;tjtfffLLGEKKWWKEDj.       
+                ,fDEEEDfii,,;jtitiff.  .;jLGEKKWKKEGDi      
+                :LDEDGLti;    ittfjj ...tjfGKKKKWKEDj,      
+               ,fDEEDDLjt,,;  ijiff;   .LffEEKWWKKELj,      
+              :fGEEEDDGjtj,  ;ti;tLff;tfjLLGEKKWKKEDGf;     
+               ,LLEEDGDjti;jttii;ttffLfiLfGEDEKWWKKDGLt     
+               .fDEEDLDLjit;ttttitjjjLtjLfGEEEWWWKWKEDGj:   
+              .iLDEEDEELjtitttttttjjfjiLjGDEKEKKWWKEDDLf;   
+               ;GDEEDEDDfiiijjtjjjffLfjffLDDEEEKWWWKEDf;.   
+             .tfGEEDDEDLftjfttjjfjf:fLLffGGEEKEKKWWWEGt:    
+            .,;fDKKDDEDGfttjjtitjtjffLfffGGEEKDEEKKWKKGi    
+             .tGEKEDDEDGfjjjjjtjjffLGLjfLGGEKKDGEEEEEEGj.   
+             :jDEEEDGEEGftjftjjjfffffffffDDEKKGGLEKEEEGf,   
+             ;fGEEDGGEEDLjtjjjtfjffLfffffDDEKDLGDDDKDDfi.   
+             .tGEDDDGDDDLj,;E..,t:::,W:,fDDEEDGGDEEEEDL:    
+             ,LGLDGDGGGGGL;iE,:,;;;;:W,,fDEEGfGGDEEEDLi.    
+            .,;jfGDDGLLLGfti;,:,;;ii;.;,LDEDLfGDDEEDLj.     
+              .tjDDDGGLfLLL,:i;;;iit;;i:GDDGLLGEDEDDLi      
+               .jfGDGGGfLLf,:t;;iiit;ti;GEDLDDDDDDDGLj,.    
+               .,tGLGGGLGfLj,ittiitj.t:GDGGDDDEDDDGGGGL:    
+                .:iLGDGLGLLf;ifttitjfj,DDGDDDDDGGGGDDGf,    
+                 .iLLGLGGGLLf:Ljtitjj;fDDGEDGLLLLGDDEEDj    
+                 .tfGLLLGGLLfiLttijtfGDDEDDGLffffGGDDEEE,.  
+                 .jGGGLLLGGLLLWGjtjLKGGDEEGfLLLffLDEEEKKG.  
+                 ;fDGGGLLGGLLfjEGGEjLLGEEDLGLLfLLLGGEEKKWL. 
+                 jEDGGLLLLGLGLjtjffffLDEDLLLGLfLGGDDEEKWWWf.
+                tEEDGLfjfLLfGDGfjjjfDDDDLfffffLLLLGEKKKKWWK;
+                DEEDGLffffffLGGLLLGGGDDLLfjjjfLLGGGDKWWKKWWK
+               jEEEDLLffLffffLLLLLLLfLLLLLLjjfLfLDEDEKWWWWWK
+               DKEEGGDfGLLffffLLGLfjjffLLfLfffLGLDDKEKWWWWWE
+              ;EEKDGDDGGGGLLffLLLLfjjffLLffLLLLGGGGEKKKWWWWE
+              LKKEDDEDDDDGLfffLGfffffjfLLfLLfLLGGGDDEKKWWWWE
+              DEEDDEEEDDDGfLLLLLfLLfjLjjjfffLLLGDGGDEKKWWWWD
+             .LDEEEEDEDDDGLLLLLfffLLtjjjfjfLfGLLGGDDEKKKWWWW
+             :tEEKEDDKDDDDGGLGLfLffjtttjjfffLLLGLDDDKKKWWWW#
+             .tEEKEEDKDDDDGGLLLLfffjtttjjjffLLLLGGDEEKKWWWW#
+              jEKKKKDEDEDDGDGGGLjfjtttttjffffLLLGLDEEKWWWW#W
+              jEEEKKDDDEGGGDGLGLfjtiitttjjjfLLLLGDDDKKKWWWW#
+              tEEEKKDDGDDGGDLLGfjjttitttjftfLGLLGGDEEEKWWWW#
+              ,DEEEEDEDEDGLGGLLLfjtiijttjfjLfGLGLGDDEKKKWWW#
+              .GEEEEEEDDDGLfDLGjfttiijtjjffjfLLGGGGDEEKKWW#W
+               fEEDEEEDDDDLLGGGLLtjiijtjjffffLfLDGDDEDKKKWW#
+               fEDGEEDEDDDGLGLLGfjjtitijjfffffffLDDDEEEKKWW#
+               LDDDDDDDDGDDGGfLLfjjjtiijjfLffjfLLDDDEEEEKWW#
+               LDEDGGDDDGGDfGfLLfjftttijfjLffjjLfDDEDDDDEKWW
+               fDEDGGGDDGGGLLffLfjfjjt;jtjLffjjGLLDDGDDDDEWW
+               jDDDLLLGDDGfLLfjfffjitiiittffjjfLfLGDDLLGDDEW
+               iDDGLLLGGGGLLLffjfftjji;ittjfjjjtffGGGGLLGEKK
+               :GDGffLGLLGfjjjjtjjjtiti;jtjftjttLffLGGGLLGEK
+               :LGGLfLLLjLfttttjtjjjjtt;jjjfttiitjffjGGGGGEE
+               .fGGLLLfjLfjtiiittjijtjt;tjtjti,,tjftjffLGGDE
+</pre>
