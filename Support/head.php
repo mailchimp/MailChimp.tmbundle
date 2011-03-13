e67fb753e11868f8e2c5990018bc40b1660b1031
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-03-13 19:45:20 -0400

    moving timing of config load to end

--- Support/head.php
+++ Support/head.php
@@ -1,11 +1,10 @@
 <?php
 // All the required files we will need
-
-include getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'config.php';
 include getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'escape.php';
 include getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'ui.php';
 include getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'error.php';
 include getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'util.php';
+include getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'config.php';
 include getenv('TM_BUNDLE_SUPPORT').DIRECTORY_SEPARATOR.'mailchimp'.DIRECTORY_SEPARATOR.'MCAPI.class.php';
 
 //Start up config + API object. we will need them everywhere.
