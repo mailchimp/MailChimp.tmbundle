79187be9dc812297e4ac7507e47969471fb1debf
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-03-18 01:24:24 -0400

    remove double init, since we now request api key in constructor

--- Support/helper/init.php
+++ Support/helper/init.php
@@ -2,5 +2,5 @@
 // How init works:
 // Config will trigger the Init if it finds it has no API Key available.
 // This is just simple when someone wants to run init explicitly
-$config->init();
+// $config->init();
 echo sprintf(__('init_success_message'), $config->api_key);
