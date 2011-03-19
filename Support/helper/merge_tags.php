07b065693a1736a972f376b7e3ae815ebe1e913b
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-03-19 07:58:25 -0400

    rearrange merge tags to be easier to modify

--- Support/helper/merge_tags.php
+++ Support/helper/merge_tags.php
@@ -1,23 +1,22 @@
 <?php
+$mergetags = array();
 // List of Default MC Merge Tags.
-// To be broken up based on feedback
-// Moved into a user editable ini?
-$mergetags = array(
-'*|ARCHIVE|*',
-'*|REWARDS_TEXT|*',
-'*|MC_TRANSLATE|*',
-'*|MC:TRANSLATE|*',
-'*|DATE|*',
-'*|COMMENTS_URL|*',
-'*|RECENT:XXX|*',
-'*|TRANSLATE:xx|*',
-'*|INTERESTED:|* *|END:INTERESTED|*',
-'*|MC:TOC|*',
-'*|POLL:|* *|END:POLL|*',
-'*|QRCOUPON:xxx:yy:zzz|*',
-'*|EMAIL_UID|*',
-'*|UNIQID|*'
-);
+// Simple PHP array. Just move the pieces around to control order,
+// and just copy to add any.
+$mergetags[] = '*|ARCHIVE|*';
+$mergetags[] = '*|REWARDS_TEXT|*';
+$mergetags[] = '*|MC_TRANSLATE|*';
+$mergetags[] = '*|MC:TRANSLATE|*';
+$mergetags[] = '*|DATE|*';
+$mergetags[] = '*|COMMENTS_URL|*';
+$mergetags[] = '*|RECENT:XXX|*';
+$mergetags[] = '*|TRANSLATE:xx|*';
+$mergetags[] = '*|INTERESTED:|* *|END:INTERESTED|*';
+$mergetags[] = '*|MC:TOC|*';
+$mergetags[] = '*|POLL:|* *|END:POLL|*';
+$mergetags[] = '*|QRCOUPON:xxx:yy:zzz|*';
+$mergetags[] = '*|EMAIL_UID|*';
+$mergetags[] = '*|UNIQID|*';
 
 $UI = new UI(getenv('DIALOG'));
 $UI->popup($mergetags);
